5546df7b384b1e29587cea79efe2a511fa8ad388
ana.pelegrini <ana.pelegrini@sou.inteli.edu.br>
2026-05-12 15:26:31 -0300

    docs(#115): Cria a tabela equipe

--- documentos/outros/migration.sql
+++ documentos/outros/migration.sql
@@ -9,3 +9,22 @@ CREATE TABLE competicao (
 );
 
 CREATE INDEX idx_competicao_data ON competicao (data);
+
+-- TABELA: equipe
+CREATE TABLE equipe (
+    id              SMALLINT        NOT NULL GENERATED ALWAYS AS IDENTITY,
+    nome            VARCHAR(100)    NOT NULL,
+    uuid            UUID            NOT NULL DEFAULT gen_random_uuid(),
+    qr_code         JSON            NULL,
+    competicao_id   SMALLINT        NOT NULL,
+    criado_em       TIMESTAMP       NOT NULL DEFAULT NOW(),
+
+    PRIMARY KEY (id),
+    UNIQUE (uuid)
+);
+
+ALTER TABLE equipe
+    ADD CONSTRAINT equipe_competicao_id_foreign
+    FOREIGN KEY (competicao_id) REFERENCES competicao (id);
+
+CREATE INDEX idx_equipe_competicao_id ON equipe (competicao_id);
