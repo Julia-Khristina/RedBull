29b1fe09072ef07928ef718984111b5c3a448a62
ana.pelegrini <ana.pelegrini@sou.inteli.edu.br>
2026-05-13 14:22:25 -0300

    docs(#116): Implementa alterações na tabela checkpoint

--- documentos/outros/migration.sql
+++ documentos/outros/migration.sql
@@ -98,3 +98,25 @@ CREATE TABLE checkpoint (
     CHECK (km >= 0)
 );
 
+
+ALTER TABLE checkpoint
+    ADD CONSTRAINT checkpoint_corredor_id_foreign
+    FOREIGN KEY (corredor_id) REFERENCES corredor (id);
+
+ALTER TABLE checkpoint
+    ADD CONSTRAINT checkpoint_competicao_id_foreign
+    FOREIGN KEY (competicao_id) REFERENCES competicao (id);
+
+ALTER TABLE checkpoint
+    ADD CONSTRAINT checkpoint_esteira_id_foreign
+    FOREIGN KEY (esteira_id) REFERENCES esteira (id);
+
+ALTER TABLE checkpoint
+    ADD CONSTRAINT checkpoint_administrador_id_foreign
+    FOREIGN KEY (administrador_id) REFERENCES administrador (id);
+
+CREATE INDEX idx_checkpoint_corredor_id       ON checkpoint (corredor_id);
+CREATE INDEX idx_checkpoint_competicao_id     ON checkpoint (competicao_id);
+CREATE INDEX idx_checkpoint_esteira_id        ON checkpoint (esteira_id);
+CREATE INDEX idx_checkpoint_administrador_id  ON checkpoint (administrador_id);
+CREATE INDEX idx_checkpoint_criado_em         ON checkpoint (criado_em);
