91dfb1c531013d4c7d6650b0153b2d64eac1e922
ana.pelegrini <ana.pelegrini@sou.inteli.edu.br>
2026-05-25 16:33:29 -0300

    feat(#259): Implementa checkpoint atleta

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express = require("express");
 import competitionRoutes from "./routes/competitionRoutes";
 import teamRoutes from "./routes/teamRoutes";
+import athleteRoutes from "./routes/athleteRoutes";
 import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(competitionRoutes);
 app.use(teamRoutes);
+app.use(athleteRoutes);
 app.use(errorHandler);
 
 export default app;
