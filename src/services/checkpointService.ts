95e32053695c9ddfb81abf2c75c20e8595262b26
Isabella Sandra Santos <isabella.santos@sou.inteli.edu.br>
2026-05-28 17:23:45 +0000

    fix: remove extra closing brace in checkpointService.ts (TS1128)

--- src/services/checkpointService.ts
+++ src/services/checkpointService.ts
@@ -96,4 +96,3 @@ import {
                                                                                                                                                                                                                                                                                                                                                                                                 }
 
                                                                                                                                                                                                                                                                                                                                                                                                 export const checkpointService = createCheckpointService();
-}
