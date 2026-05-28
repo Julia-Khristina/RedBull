0f774830778dbfc6239ccad67aaa158ab9c12838
Isabell Santos <Isabella.santos@sou.inteli.edu.br>
2026-05-28 14:38:34 -0300

    fix: correcao código anexado na branch

--- src/validators/checkpointValidator.ts
+++ src/validators/checkpointValidator.ts
@@ -141,4 +141,3 @@ import {
 
                                                                                                                                                                                                                                                                                                                                   return result;
                                                                                                                                                                                                                                                                                                                                   }
-}
